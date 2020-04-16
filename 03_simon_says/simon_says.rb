3d5de62ffe9283ce29116745a73df0cbe6a72b78
Derek Baker <derek.baker89@gmail.com>
2020-04-16 23:42:31 +0100

    Pass fourth exercise

--- 03_simon_says/simon_says.rb
+++ 03_simon_says/simon_says.rb
@@ -1,1 +1,31 @@
-#write your code here
+def echo(word)
+  "#{word}"
+end
+
+def shout(word)
+  "#{word}".upcase
+end
+
+def repeat(word, num=2)
+  ( "#{word} " * num ).strip
+end
+
+def start_of_word(word, num)
+  word[0 ... num]
+end
+
+def first_word(word)
+  word.partition(" ").first
+end
+
+def titleize(phrase)
+  words = phrase.split.map do |word|
+    if %w( the and over ).include?(word)
+      word
+    else
+      word.capitalize
+    end
+  end
+  words.first.capitalize!
+  words.join(" ")
+end

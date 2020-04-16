d92a3e9af2e149055dd84d986cdda84ff07961f0
Derek Baker <derek.baker89@gmail.com>
2020-04-16 01:36:35 +0100

    Pass third exercise

--- 02_calculator/calculator.rb
+++ 02_calculator/calculator.rb
@@ -1,1 +1,19 @@
-#write your code here
+def add(num1, num2)
+  num1 + num2
+end
+
+def subtract(num1, num2)
+  num1 - num2
+end
+
+def sum(array)
+  if array == [] 
+    0
+  else
+    array.reduce(:+)
+  end
+end
+
+def multiply(*num)
+  num.reduce(:*)
+end
--- 02_calculator/calculator_spec.rb
+++ 02_calculator/calculator_spec.rb
@@ -77,12 +77,16 @@ end
 # once the above tests pass,
 # write tests and code for the following:
 
-describe "#multiply" do
+describe "multiply" do
 
-  it "multiplies two numbers"
+  it "multiplies two numbers" do
+  expect(multiply(2,2)).to eq(4)
+  end
 
-  it "multiplies several numbers"
-  
+  it "multiplies several numbers" do
+  expect(multiply(10,10,10)).to eq(1000)
+  end
+
 end
 
 describe "#power" do
